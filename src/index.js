125b6843f9608ec2436e6e45040feeb7c8c9ec61
lugovczov <vova.lugovczov@mail.ru>
2020-02-25 19:49:52 +0300

    feat: add solution this task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
+  let arr = n.toString().split('');
+  let arrRes = [];
   
+  if (arr[0] === "-" ) {
+      arr.shift(arr[0]);
+  }
+  
+    for (let key of arr) {
+      arrRes.unshift(key);
+  }
+
+  return arrRes.join('');
 }
